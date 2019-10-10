d36201d8880fb3210a293f3540152d2c05bcfa45
Glenn Carremans <glenn.carremans@appwise.be>
2019-10-10 12:22:08 +0200

    Reproduce bug

--- app/src/main/java/com/leshchenko/circularlayoutmanager/MainActivity.kt
+++ app/src/main/java/com/leshchenko/circularlayoutmanager/MainActivity.kt
@@ -15,13 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         circularRecycler.layoutManager = CircularRecyclerLayoutManager(
-            canScrollHorizontally = true,
-            itemsPerCircle = 4,
-            anglePerItem = 100.0,
+            canScrollHorizontally = false,
+            itemsPerCircle = 5,
+            anglePerItem = 45.0,
             firstCircleRadius = 200.0,
-            angleStepForCircles = 45.0,
-            isClockwise = false,
-            initialAngle = 180.0
+            angleStepForCircles = 0.0,
+            isClockwise = true,
+            initialAngle = 270.0
         )
         circularRecycler.adapter = Adapter()
     }
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
             ItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false))
 
-        override fun getItemCount() = 200
+        override fun getItemCount() = 5
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {}
 
